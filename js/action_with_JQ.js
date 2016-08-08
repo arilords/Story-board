dab99945f19d4b5502616e3ae8aa17fb40cadd77
arilords <arilords@gmail.com>
2016-08-08 11:53:45 -0500

    fixed all of JQ

--- js/action_with_JQ.js
+++ js/action_with_JQ.js
@@ -1,80 +1,79 @@
 // variables
-var counter = 2;
+var counter = 0;
+var input = $("input").val()
+
+if(counter === 0){
+$("#speechone").html("<p> Hello </p>");
+$("#speechone").css('display', 'block');
+$("#speechtwo").html("<p> Hello </p>");
+$("#speechtw").css('display', 'block');
+}
+else{
+  // runDialog();
+}
 
 $("#return").click(function(){
-  counter = 2
+  counter = 0;
   console.log(counter);
   runDialog();
 });
 
 $("#continue").click(function(){
-  counter = counter + 1
+  counter = counter + 1;
   console.log(counter);
   runDialog();
 });
 
-$("#speechone").append("<p> Hello </p>");
-$("#speechtwo").append("<p> Hello </p>");
-
 function runDialog(){
     switch(counter) {
+        case 1:
+          $("#speechone").html("<p> How you doin? </p>");
+          break;
         case 2:
-          speechone.innerHTML = "<p> How you doin? </p>";
+          $("speechtwo").html("<p> I'm all good bro! </p>");
           break;
         case 3:
-          speechtwo.innerHTML = "<p> I'm all good bro! </p>";
+          $("#speechone").html("<p> What chu feel like doin today? </p>");
           break;
-
         case 4:
-          speechone.innerHTML = "<p> What chu feel like doin today? </p>";
-          break;
-        case 5:
-          speechtwo.innerHTML = "<p> What did you have planned? </p>";
-          break;
-        case 6:
-          speechone.innerHTML = "<p> We can stay here, go to the park, we can even go watch a movie. </p>";
-          $('body').keypress(function(e){
-            if(e.keycode == 32){
-              speechone.innerHTML= "<p> Any of those sound interesting to you? </p>";
-            }
-          })
+          $("#speechtwo").html("<p> What did you have planned? </p>");
           break;
 
-          // Make this a prompt for the reader
-        case 7:
-          speechtwo.innerHTML = "<p>  </p>";
-          speechtwo.style.display = "block";
-          speechtwo.style.display = "none";
-          alert("line 47");
-          break;
+         case 5:
+           $("#speechone").html("<p> We can stay here, go to the park, we can even go watch a movie. </p>");
+           break;
 
-        // Based on their answer of staying, going to the park, or going ot see a movie, change the background accordingly.
-        case 8:
-          speechone.innerHTML = "<p> What chu feel like doin today </p>";
-          speechone.style.display = "block";
-          speechtwo.style.display = "none";
-          alert("line 55");
-          break;
-        default:
-          speechtwo.innerHTML = "<p> Speech Complete </p>";
-          speechtwo.style.display="block";
-          speechone.innerHTML = "<p> Speech Complete </p>";
+           // Make this a prompt for the reader
+         case 6:
+            $("#speechtwo").html("<input></input>");
+             $("#speechtwo").html(input);
+           break;
+
+         // Based on their answer of staying, going to the park, or going ot see a movie, change the background accordingly.
+         case 7:
+           $("#speechone").html("<p> Alright then let's go. </p>");
+           $("#speechone").css('display', 'block');
+           $("#speechtwo").css('display', 'none');
+           break;
+
+         default:
+           $("#speechtwo").html("<p> Speech Complete </p>");
+           $("#speechtwo").css('display', 'block');
+          $("#speechone").html("<p> Speech Complete </p>");
     }
+    var even_or_odd = counter % 2;
+
+        if(even_or_odd === 1){
+          $("#speechone").attr('style', 'display:block;');
+          $("#speechtwo").attr('style', 'display:none;');
+        }
+
+        else if(even_or_odd === 0){
+          $("#speechtwo").attr('style', 'display:block;');
+          $("#speechone").attr('style', 'display:none;');
+        }
+        else{
+          alert("ERROR");
+        }
+
 }
-
-var even_or_odd = counter % 2;
-
-    if(even_or_odd === 0){
-      speechone.style.display = "block";
-      speechtwo.style.display = "none";
-      alert("line 69");
-    }
-
-    else if(even_or_odd === 1){
-      speechtwo.style.display = "block"
-      speechone.style.display = "none";
-      alert("line 75");
-    }
-    else{
-      alert("ERROR");
-    }
